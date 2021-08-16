c33627fa1f094662b06e51274e264c74e28f9022
Jan Dolanský <dolanovsky@gmail.com>
2021-08-16 21:33:59 +0200

    Add amount of results when searching, begin notification fetching functionality

--- static/index.js
+++ static/index.js
@@ -1,30 +1,101 @@
-const items = document.getElementsByClassName("track")
+/**
+ * Search
+ */
 
-document.getElementById("search").addEventListener('input', function(e) {
+const items = document.getElementsByClassName('track')
+// TODO: Implement showing of results & 'nothing found'
+const resultsEl = document.getElementById('results')
+
+document.getElementById('search').addEventListener('input', function (e) {
   const search = document.getElementById('search').value
 
-  if (search !== "") {
-    for (var i = 0; i < items.length; i++) {
-        const item = items[i];
-        const attrs = item.getAttribute('data-keyword') ?? "";
+  if (search !== '') {
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i]
+      const attrs = item.getAttribute('data-keyword') ?? ''
 
-        if (attrs.toLowerCase().includes(search.toLowerCase())) {
-          item.style.display = "block";
-        } else {
-          item.style.display = "none";
-        }
+      if (attrs.toLowerCase().includes(search.toLowerCase())) {
+        item.style.display = 'block'
+      } else {
+        item.style.display = 'none'
       }
-    } else showAll(items)
+    }
+  } else showAll(items)
+
+  // Run through visible items and count results
+
+  let results = 0
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i]
+    if (item.offsetParent !== null) results++
+  }
+
+  if (results < items.length) {
+    // Show results
+    setResults(`Found <strong>${results}</strong> record(s)`, 'block')
+  } else {
+    // Hide results
+    setResults('', 'none')
+  }
 })
 
-document.getElementById('clear').addEventListener("click", function(e) {
-  document.getElementById('search').value = "";
+document.getElementById('clear').addEventListener('click', function (e) {
+  document.getElementById('search').value = ''
   showAll(items)
 })
 
+function setResults(inner, display) {
+  resultsEl.innerHTML = inner
+  resultsEl.style.display = display
+}
 
 function showAll(items) {
   for (var i = 0; i < items.length; i++) {
-    items[i].style.display = "block"
+    items[i].style.display = 'block'
   }
+  setResults('', 'none')
 }
+
+/**
+ * Add notifications since last login
+ */
+
+async function fetchRecords() {
+  // Get the time when data was last fetched
+  const since = localStorage.getItem('since')
+
+  await fetch('url', {
+    method: 'POST',
+    body: JSON.stringify({ since: since }),
+  })
+    .then((response) => {
+      const data = response.json()
+      // New records since last fetch?, render new notifications
+      if (data) renderAlerts(data)
+    })
+    .catch((e) => {
+      console.log('err:')
+      alert('Error fetching data. Fuck off.')
+    })
+    .finally(() => {
+      // Get current fetch timestamp and save it
+      const now = new Date()
+      localStorage.setItem('since', now)
+    })
+}
+
+function renderAlerts() {
+  // First clear all previous notifications
+
+  // Render new notifications
+  console.log('piss off')
+}
+
+// Runtime
+// fetchRecords()
+
+// const FETCH_TIMEOUT = 300000
+
+// const interval = setInterval(() => {
+//   fetchRecords()
+// }, FETCH_TIMEOUT)
