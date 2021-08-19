ee4c2bfd4c8a48d79d4845caf10a6dd886163186
Jokler <jokler@protonmail.com>
2021-08-20 01:20:54 +0200

    Fix name parser in some cases & map colors to u8

--- src/parse.rs
+++ src/parse.rs
@@ -88,7 +88,8 @@ impl Tag {
         let g = u8::from_str_radix(g, 16).unwrap();
         let b = u8::from_str_radix(b, 16).unwrap();
 
-        Self::Color(r, g, b)
+        // Copy lower half of the byte into upper half
+        Self::Color(r | r << 4, g | g << 4, b | b << 4)
     }
 }
 
@@ -98,37 +99,45 @@ mod tests {
     use Tag::*;
 
     #[test]
-    fn pest() {
+    fn fortuna_atraction() {
         let input = "$w$s$i$a$afa$afgFortuna atra$wction";
 
-        let actual = map_name(input);
+        let actual = map_name(input).unwrap();
 
         let expected = vec![
-            Fragment::Tag(
-                Wide,
-            ),
-            Fragment::Tag(
-                Shadowed,
-            ),
-            Fragment::Tag(
-                Italic,
-            ),
-            Fragment::Tag(
-                Color(
-                    10,
-                    15,
-                    10,
-                ),
-            ),
-            Fragment::Text(
-                "Fortuna atra",
-            ),
-            Fragment::Tag(
-                Wide,
-            ),
-            Fragment::Text(
-                "ction",
-            ),
+            Fragment::Tag(Wide),
+            Fragment::Tag(Shadowed),
+            Fragment::Tag(Italic),
+            Fragment::Tag(Color(170, 255, 170)),
+            Fragment::Text("Fortuna atra"),
+            Fragment::Tag(Wide),
+            Fragment::Text("ction"),
+        ];
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn fruzzy() {
+        let input = "$f0f$w$s$xF$f1fr$f2fu$f4fz$f6fz$f8fy";
+
+        let actual = map_name(input).unwrap();
+
+        let expected = vec![
+            Fragment::Tag(Color(255, 0, 255)),
+            Fragment::Tag(Wide),
+            Fragment::Tag(Shadowed),
+            Fragment::Text("F"),
+            Fragment::Tag(Color(255, 17, 255)),
+            Fragment::Text("r"),
+            Fragment::Tag(Color(255, 34, 255)),
+            Fragment::Text("u"),
+            Fragment::Tag(Color(255, 68, 255)),
+            Fragment::Text("z"),
+            Fragment::Tag(Color(255, 102, 255)),
+            Fragment::Text("z"),
+            Fragment::Tag(Color(255, 136, 255)),
+            Fragment::Text("y"),
         ];
 
         assert_eq!(expected, actual);
